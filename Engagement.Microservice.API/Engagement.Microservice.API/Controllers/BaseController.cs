96bbf595b3634232812816dd33755bbaadcda2dc
rezzny4 <ivan.burgard@gmail.com>
2025-05-31 11:24:28 +0300

    Setup

---
+++ Engagement.Microservice.API/Engagement.Microservice.API/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Engagement.Microservice.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IMediator _mediator;
+
+        public BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
